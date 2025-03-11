263f5116b85ddebe8f64cfe203e1f1a8bc6cc325
oneirodyne <oneirodynia@yahoo.com>
2025-03-10 17:34:34 -0700

    First addition!

---
+++ romanizer.c
@@ -0,0 +1,32 @@
+/* This program will 'romanize' names by adding 'ius' when it detects certain
+keywords								   */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+int main(int argc, char *argv[]){
+
+// Make a pointer to the first argument
+	char* name = argv[1];
+
+/* Detect if the string has these characters right before the end :
+>i
+>io
+>y
+*/
+	int i;
+	int o;
+	int len = strlen(name);
+	char nameOut[20];
+
+	for(i=0;i<len;i++){
+	name[i] = toupper(name[i]);
+	if(name[i] == 'U'){
+	name[i] = 'V';}
+	}
+// Print the converted string
+
+	printf("%s\n", name);
+
+}
